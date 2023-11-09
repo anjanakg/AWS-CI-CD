fdb32834e29cb17093e6e333a4379cf869263650
Anjana Gunathilake <86033987+anjanakg@users.noreply.github.com>
2023-11-09 14:05:57 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,9 +18,9 @@ def get_requirements(file_path:str)->List[str]:
 
 setup(
 name='mlproject',
-version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
+version='v1',
+author='Anjana',
+author_email='anjanakg@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
